4a8dc63cda7256d1f808689959ed79d5c2b0f75d
faustovaz <faustovaz@a0281dac-b656-4528-9534-7540dec8f65c>
2012-09-21 04:47:02 +0000

    Implementando o envio de arquivos (javascript)

--- mockup/html/js/tts.js
+++ mockup/html/js/tts.js
@@ -1,17 +1,46 @@
 $(document).ready(function(){
 
 	$("#show-files-to-upload").addClass('display-off');
+	$("#show-files-to-upload-background").addClass('display-off');
+
+
 	$("input[name='files[]']").bind('change', function(evt){
-		var files = evt.target.files;
-		var html = "";
-		for (var i = 0; i < files.length; i++){
-			var file = files[i];
-			html = html + "<span>" + file.name + "</span><span>" + file.size + "</span>";
+
+		var selectedFiles = evt.target.files;
+		var file;
+		var html="";
+		var readableFileSize;
+		for (var i = 0; i < selectedFiles.length; i++){
+			file = selectedFiles[i];
+			if ((file.size / 1024 ) > 1024){
+				readableFileSize = ((file.size / 1024) / 1024) + 'MB';
+			}
+			else{
+				readableFileSize = (file.size / 1024) + 'KB';
+			}
+			html = html + '<li>';
+			html = html + '<div class=\'file-image\'><img src=\'img/unknown.png\'/></div>';
+			html = html + '<div class=\'file-name\'>' + file.name + '</div>';
+			html = html + '<div class=\'file-size\'>' + readableFileSize + '</div>';
+			html = html + '<div class=\'file-download\'>Enviar</div>';
+			html - html + '</li>';
 		}
 
-		$("#show-files-to-upload").html(html);
+		$('#list-selected-files').html(html);
+		
 		$("#show-files-to-upload").removeClass('display-off');
 		$("#show-files-to-upload").addClass('display-on');
+
+		$('#show-files-to-upload-background').removeClass('display-off');
+		$('#show-files-to-upload-background').addClass('display-on');
+	});
+
+	$('#show-files-to-upload-title-close').bind('click', function(evt){
+		$("#show-files-to-upload").removeClass('display-on');
+		$("#show-files-to-upload").addClass('display-off');
+
+		$('#show-files-to-upload-background').removeClass('display-on');
+		$('#show-files-to-upload-background').addClass('display-off');
 	});
 });
 
